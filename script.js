8a8017107fe4e5711dab53569a0bd3e6f26b1a73
Ryuichi Shimajiri <>
2020-10-26 20:30:36 +0900

    練習しました。

--- script.js
+++ script.js
@@ -2,22 +2,22 @@
 // 問題文はindex.htmlにあります。
 
 
-// Q 1
+// // Q 1
 let moji = document.getElementById("q1-text").textContent;{
 	console.log(moji);
 }
 
-// Q 2
+// // Q 2
 
 let moji1 = document.getElementById('q2-text');
 moji1.textContent = "テキストを変更しました。";
 
 
-// Q 3
+// // Q 3
 document.getElementById("q3-text").style.color="red";
 
-// Q 4 (難)
-// 「for of」を使います。
+// // Q 4 (難)
+// // 「for of」を使います。
 
 
 
@@ -26,10 +26,11 @@ for (let iro of list){
 		iro.style.color = "red";
 }
 
-// Q 5
+// // Q 5
 
  let list2 = document.getElementsByClassName("q5-text");
 for (let iro1 of list2){
 		iro1.style.backgroundColor = "red";
 }
+
 
